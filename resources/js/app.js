90b717491e0284f60395d1d207ba1d685e2a0441
cleisonsantos <css.cleison@outlook.com>
2020-11-29 14:25:11 -0300

    Adicionado o vue router.

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,12 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import VueRouter from 'vue-router';
 
+import Home from './components/HomeComponent'
+import Example from './components/ExampleComponent'
+
+Vue.use(VueRouter)
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -19,14 +24,30 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('home-component', require('./components/HomeComponent.vue').default);
+/* Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('home-component', require('./components/HomeComponent.vue').default); */
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/teste',
+            name: 'teste',
+            component: Example
+        }
+    ]
+})
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    router
 });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+
+<router-view></router-view>
+
 <!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -20,7 +23,5 @@
         </div>
     </div>
 </div> -->
-
-<home-component></home-component>
 
 @endsection
