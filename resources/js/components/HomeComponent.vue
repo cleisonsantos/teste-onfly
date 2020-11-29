1a2dac17994d75a4f6b57d4c25f35434f14a2cd6
cleisonsantos <css.cleison@outlook.com>
2020-11-29 12:09:59 -0300

    Login do laravel e o vuejs aplicados no projeto.

---
+++ resources/js/components/HomeComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md">
+                <div class="card">
+                    <div class="card-header">Home</div>
+
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {},
+    data() {
+        return { 
+            message: 'Hello Teste'
+        }
+    }
+}
+</script>
